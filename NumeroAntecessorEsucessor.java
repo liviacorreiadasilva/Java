a7ce8e9e01739dc156e95b48ef1a8750006152c2
Livia Correia da Silva <liviasilvasilva492@uni9.edu.br>
2024-04-05 17:20:11 -0300

    Create NumeroAntecessorEsucessor.java

---
+++ NumeroAntecessorEsucessor.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+int num = 4;
+int antecessor = num -1;
+int sucessor = num + 1;
+System.out.println("Saída: " + antecessor + ", " + sucessor);
+  }
+}
