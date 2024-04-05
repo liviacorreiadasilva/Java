e2a426ff3e2bf1ce81f7898311815e37aa68f5cb
Livia Correia da Silva <liviasilvasilva492@uni9.edu.br>
2024-04-05 16:24:18 -0300

    Create QuantosMesesEdiasDeVida.java

---
+++ QuantosMesesEdiasDeVida.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int anos = 19;
+        int meses = 228;
+        int dias = 6840; // Corrigido para um valor inteiro
+
+        int total = (anos * 365) + (meses * 30) + dias;
+        System.out.println("Idade em dias: " + total);
+    }
+}
