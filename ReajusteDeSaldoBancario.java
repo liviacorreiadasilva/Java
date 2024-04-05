f848dc9c62242415422169ad6f8530d1f5de684c
Livia Correia da Silva <liviasilvasilva492@uni9.edu.br>
2024-04-05 17:18:27 -0300

    Create ReajusteDeSaldoBancario.java

---
+++ ReajusteDeSaldoBancario.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+double saldo = 1300;
+
+System.out.println("O seu saldo de R$ " + saldo + " teve um Reajuste de 1%.");
+System.out.println("Saldo corrigido para: " + saldo * 1.01);
+
+  }
+}
