23c8728775bde335510b284c937bc29816c878cb
Livia Correia da Silva <liviasilvasilva492@uni9.edu.br>
2024-04-05 16:47:37 -0300

    Create QuantosDiasVividos.java

---
+++ QuantosDiasVividos.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        int anos = 19;
+        int meses = 228;
+        int dias = 6840; // Corrigido para um valor inteiro
+
+        int total = (anos * 365) + (meses * 30) + dias;
+        
+      
+        
+        System.out.println("você tem " + anos + " anos e " + meses + " meses. Sendo assim, você já viveu por "  + total + " dias.");
+        
+    }    
+}
